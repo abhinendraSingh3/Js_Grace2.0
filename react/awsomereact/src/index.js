002135e609fbdaa73f432e35dc1f13ff3deed6ce
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2023-03-07 11:08:18 +0530

    Update index.js

--- react/awsomereact/src/index.js
+++ react/awsomereact/src/index.js
@@ -8,12 +8,15 @@ import Card from './Card'
 ReactDom.render(
 
 <>
+
 <Navbar/>
-<Card title img="./im/Stranger Things _ une saison 4 bien plus sombre et horrifique que les précédentes [VERDICT].jpeg"/>
+<Card title="Stranger Things" img="./im/Stranger Things _ une saison 4 bien plus sombre et horrifique que les précédentes [VERDICT].jpeg"/>
 <Card title="Dahmer" img="./im/Dahmer Monster_ The Jeffery Dahmer Story.jpeg"/>
 <Card title="Bridgerton "img="./im/Bridgerton.png"/>
 <Card title="Wednesday" img="./im/Wednesday.jpeg"/>
 <Card title="Naruto" img="./im/na.jpeg"/>
+
+
 </>,
 document.getElementById('root')
 
