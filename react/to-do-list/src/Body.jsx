0c91c438dc69f3651eaaf8a2e67e5e206a552ca4
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2023-03-12 23:04:10 +0530

    initiated ooN2

---
+++ react/to-do-list/src/Body.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Body = () => {
+    const [setN, updateN] = useState();
+    const [btnS, btnUpdate] = useState();
+
+
+
+    const addI = (event) => {
+        let newEntry={note:setN}
+        btnUpdate([newEntry,btnS])
+        
+
+    }
+
+    return (
+        <div className="mainCont">
+            <div id="addIt">
+                <input type={Text}
+                    placeholder="Type here"
+                    id='iNp'
+                    onChange={(e)=>{updateN(e.target.value)}}
+                    value={setN}
+                >
+
+                </input>
+                <button typeof="submit" onClick={addI}>+</button>
+            </div>
+            <div id='writtenC'>
+                <p>{btnS}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Body
