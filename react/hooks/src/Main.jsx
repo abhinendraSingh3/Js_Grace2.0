5d7936ac07a80358fdf0a91baddcdad6ea15b561
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2023-05-25 22:46:46 +0530

    Update Main.jsx

--- react/hooks/src/Main.jsx
+++ react/hooks/src/Main.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Main=()=>{
+    const[color,setColor]=useState("red");
 return(
-<h1>hello</h1>
+<>
+<h1>hello now color is {color}</h1>
+<button type="button" onClick={()=>{setColor("blue")}}>
+Blue
+</button>
+<button type="button" onClick={()=>{setColor("Green")}}>
+Green
+</button>
+<button type="Submit" onClick={()=>{setColor("Yellow")}}>
+Yellow
+</button>
+</>
 )
 }
 export default Main
