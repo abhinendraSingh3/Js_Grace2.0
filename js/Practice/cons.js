1aefbe8c41774dbdc8f6a2556680bd4328935b84
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2025-03-04 22:28:18 +0530

    Form validation

---
+++ js/Practice/cons.js
@@ -0,0 +1,27 @@
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(5,6));
+
+// let area=(a,b)=>{
+//     console.log(a*b);
+    
+// }
+
+// area(5,6)
+
+let str="hello world";
+let reversedstr=""
+
+// function revstr(str){
+// return str.split("").reverse().join("");
+// }
+// console.log(revstr(str))
+
+for(let j=str.length-1; j>0;j--){
+    reversedstr+=str[j];    
+     
+}
+console.log(reversedstr);
+
