1144286d5342148b2cd1deccc0c1d452422e43b5
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2025-03-18 00:25:18 +0530

    ExpenseTracker

--- ExpenseTracker/expense.js
+++ ExpenseTracker/expense.js
@@ -1,17 +1,45 @@
-const expenseForm=document.getElementById('desc');
-const expType= document.getElementById('exType');
-const amount=document.getElementById('amount');
+
 const button=document.getElementById('btn');
+const form=document.getElementById('expenseTracker')
 
-button.addEventListener('click',()=>{
-
+button.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const expenseForm=document.getElementById('desc').value.trim()
+    const expType= document.getElementById('exType').value
+    const amount=document.getElementById('amount').value.trim()
+    const tbody=document.querySelector('tbody')
     if(expenseForm && expType && amount){
-        let typeV=expType.value
-        let descV=expenseForm.value
-        let amtV=amount.value
-        
 
-        console.log(typeV,descV,amtV)
+        const newTr=document.createElement('tr')
+
+        // const newExDes=document.createElement('td')
+        // const newExType=document.createElement('td')
+        // const newAmt=document.createElement('td')
+
+        // console.log(newExDes);
+        
+        // newExDes.innerText=expenseForm
+        // newExType.innerText=expType
+        // newAmt.innerText=amount
+
+        // tbody.appendChild(newTr)
+        // newTr.appendChild(newExDes)
+        // newTr.appendChild(newExType)
+        // newTr.appendChild(newAmt)
+
+        //-------------------------or-------------
+
+        newTr.innerHTML=`<td>${expenseForm}</td> <td>${expType}</td> <td>${amount}</td>`
+
+        tbody.appendChild(newTr)
+
+        form.reset();
+        
+        
+    }
+
+    else{
+        alert("please fill all")
     }
 
 
