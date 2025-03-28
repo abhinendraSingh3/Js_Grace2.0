f1415e14e23c07f1c1c8f725c76e861dbf12746b
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2025-03-28 20:19:13 +0530

    pr-ms

--- react/practise/src/App.js
+++ react/practise/src/App.js
@@ -1,21 +1,55 @@
 import React, { useState } from 'react';
 function App(){
+ 
+  
+  const[name,setName]=useState("")
+  const[email,setEmail]=useState("")
+  const[pass,setPass]=useState("")
 
-  const [count,setCount]=useState(0);
+  const eventhandlename=(en)=>{
+    
+      console.log(en.target.value)
+      setName(en.target.value)
+    
+  }
+  const eventhandleemai=(em)=>{
+    
+    console.log(em.target.value)
+    setEmail(em.target.value)
+  
+}
+const eventhandlepas=(ep)=>{
+    
+  console.log(ep.target.value)
+  setPass(ep.target.value)
 
-  const inc=()=>{
-    setCount(count+1);
+}
+  const handleForm=(e)=>{
+    e.preventDefault();
+    
+
   }
 
   return(
-    <div>
-      <h1>{count}</h1>
-      <button onClick={inc}>+</button>
-      
-    </div>
+    <>
+    <form onSubmit={handleForm}>
+      <label>username: </label>
+      <input type="text" placeholder="Enter your username" name="username" onChange={eventhandlename} value={name}></input><br></br>
 
+      <label>email: </label>
+      <input type="email" placeholder="Enter your email" name="email" onChange={eventhandleemai} value={email}></input><br></br>
+
+      <label>password: </label>
+      <input type="Password" placeholder="Enter your password" name="password" onChange={eventhandlepas} value={pass}></input><br></br>
+
+      <button type="submit">Submit</button>
+    </form>
+    
+  </>
+    
 
   )
+
 }
 
 export default App
