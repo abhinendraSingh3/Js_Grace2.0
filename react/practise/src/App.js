164eb14f4d229730738c4cd35b30195e6bc5fcd3
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2025-03-31 21:27:41 +0530

    Update App.js

--- react/practise/src/App.js
+++ react/practise/src/App.js
@@ -2,28 +2,32 @@ import React, { useState } from 'react';
 function App(){
  
   
-  const[name,setName]=useState("")
-  const[email,setEmail]=useState("")
-  const[pass,setPass]=useState("")
+  const[formData,setformData]=useState({
+    username:"",
+    email:"",
+    password:"",
 
-  const eventhandlename=(en)=>{
-    
-      console.log(en.target.value)
-      setName(en.target.value)
-    
+
+  })
+  // const[email,setEmail]=useState("")
+  // const[pass,setPass]=useState("")
+
+  const eventhandleForm=(en)=>{
+    const {name,value} =en.target //The brackets {} on the left mean "extract these properties from the object on the right".
+    setformData({...formData,[name]:value})
   }
-  const eventhandleemai=(em)=>{
+//   const eventhandleemai=(em)=>{
     
-    console.log(em.target.value)
-    setEmail(em.target.value)
+//     console.log(em.target.value)
+//     setEmail(em.target.value)
   
-}
-const eventhandlepas=(ep)=>{
+// }
+// const eventhandlepas=(ep)=>{
     
-  console.log(ep.target.value)
-  setPass(ep.target.value)
+//   console.log(ep.target.value)
+//   setPass(ep.target.value)
 
-}
+// }
   const handleForm=(e)=>{
     e.preventDefault();
     
@@ -34,13 +38,13 @@ const eventhandlepas=(ep)=>{
     <>
     <form onSubmit={handleForm}>
       <label>username: </label>
-      <input type="text" placeholder="Enter your username" name="username" onChange={eventhandlename} value={name}></input><br></br>
+      <input type="text" placeholder="Enter your username" name="username" onChange={eventhandleForm} value={formData.username}></input><br></br>
 
       <label>email: </label>
-      <input type="email" placeholder="Enter your email" name="email" onChange={eventhandleemai} value={email}></input><br></br>
+      <input type="email" placeholder="Enter your email" name="email" onChange={eventhandleForm} value={formData.email}></input><br></br>
 
       <label>password: </label>
-      <input type="Password" placeholder="Enter your password" name="password" onChange={eventhandlepas} value={pass}></input><br></br>
+      <input type="Password" placeholder="Enter your password" name="password" onChange={eventhandleForm} value={formData.password}></input><br></br>
 
       <button type="submit">Submit</button>
     </form>
